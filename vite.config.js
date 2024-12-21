0867d6c71961697e871398d13cc3efdde9815174
Abhiman-Singh <124167172+Abhiman-Singh@users.noreply.github.com>
2024-12-22 01:09:49 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // base: "Dishcovery",
-  optimizeDeps: {
-    include: ['@clerk/clerk-react', 'firebase/firestore', 'react-icons/fa'],
-  },
   build: {
     rollupOptions: {
       external: ['@clerk/clerk-react', 'firebase/firestore', 'react-icons/fa'],
     },
-    assetsInlineLimit: 0,  // Ensures large assets are not inlined and are included as files in the output
   },
 });
-
-
