b7d638f2cee393d93bb6a93f6c2783dcfa4196a0
Abhiman Singh <abhimansingh165@gmail.com>
2024-12-22 09:51:41 +0530

    updation

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Dishcovery/",
+  // base: '/Dishcovery/',
   build: {
     rollupOptions: {
       external: ['@clerk/clerk-react', 'firebase/firestore', 'react-icons/fa'],
